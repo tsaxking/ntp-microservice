c8251a42510a80756af67ef6e29c86a57faf2a16
Taylor King <taylorking206@u.boisestate.edu>
2025-07-29 14:37:59 -0600

    use setInterval

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { Redis } from './services/redis';
 import { Loop } from 'ts-utils/loop';
 
 Redis.connect().unwrap().then(() => {
-    const l = new Loop(async () => {
+    const fn = async () => {
         const time = await getTime();
         if (time.isErr()) {
             console.error('Error fetching time:', time.error);
         } else {
             Redis.emit('time', time.value);
         }
-    }, 15 * 60 * 1000);
-
-    l.start();
+    };
+    fn();
+    setInterval(fn, 1000 * 60 * 15); // Every 15 minutes
 });
